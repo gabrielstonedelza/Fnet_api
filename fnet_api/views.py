dfab143a7f932e69a3fd981a5f2f5be2d7622d3f
gabrielstonedelza <gabrielstonedelza@gmail.com>
2021-12-30 08:19:50 +0000

    fetching all momo customers

--- fnet_api/views.py
+++ fnet_api/views.py
@@ -700,7 +700,7 @@ def get_all_momo_withdraw_customers(request):
 @permission_classes([permissions.IsAuthenticated])
 def get_cash_deposits_for_today(request):
     my_date = datetime.today()
-    your_cash_requests = CashDeposit.objects.filter(agent=request.user).filter(date_created=my_date.date()).order_by('-date_requested')
+    your_cash_requests = CashDeposit.objects.filter(agent=request.user).filter(date_requested=my_date.date()).order_by('-date_requested')
     serializer = CashDepositSerializer(your_cash_requests, many=True)
     return Response(serializer.data)
 
@@ -708,6 +708,6 @@ def get_cash_deposits_for_today(request):
 @permission_classes([permissions.IsAuthenticated])
 def get_bank_deposits_for_today(request):
     my_date = datetime.today()
-    your_cash_requests = BankDeposit.objects.filter(agent=request.user).filter(date_created=my_date.date()).order_by('-date_requested')
+    your_cash_requests = BankDeposit.objects.filter(agent=request.user).filter(date_requested=my_date.date()).order_by('-date_requested')
     serializer = BankDepositSerializer(your_cash_requests, many=True)
     return Response(serializer.data)
