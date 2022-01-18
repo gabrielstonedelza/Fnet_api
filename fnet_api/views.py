2fe8cc283b826bf4a5c7fe9e17d108f85f2786eb
gabrielstonedelza <gabrielstonedelza@gmail.com>
2022-01-18 08:52:48 +0000

    added deposit paid flag to models

--- fnet_api/views.py
+++ fnet_api/views.py
@@ -829,3 +829,19 @@ def approve_cash_deposit_paid(request, id):
         serializer.save()
         return Response(serializer.data)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET'])
+@permission_classes([permissions.IsAuthenticated])
+def get_upaid_cash_deposits_for_today(request):
+    my_date = datetime.today()
+    your_cash_requests = CashDeposit.objects.filter(agent=request.user).filter(date_requested=my_date.date()).filter(deposit_paid="Not Paid").order_by('-date_requested')
+    serializer = CashDepositSerializer(your_cash_requests, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+@permission_classes([permissions.IsAuthenticated])
+def get_upaid_bank_deposits_for_today(request):
+    my_date = datetime.today()
+    your_cash_requests = BankDeposit.objects.filter(agent=request.user).filter(date_requested=my_date.date()).filter(deposit_paid="Not Paid").order_by('-date_requested')
+    serializer = BankDepositSerializer(your_cash_requests, many=True)
+    return Response(serializer.data)
