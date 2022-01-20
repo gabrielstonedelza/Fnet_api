e7e193f1f158dfd3c09c55f616f3596c60aa5654
gabrielstonedelza <gabrielstonedelza@gmail.com>
2022-01-20 03:38:15 +0000

    removed additional payment option from model

--- fnet_api/models.py
+++ fnet_api/models.py
@@ -11,8 +11,19 @@ ID_TYPES = (
     ("Drivers License","Drivers License"),
     ("Voters Id","Voters Id"),
 )
-BANKS = (
-    ("Select bank", "Select bank"),
+BANKS1 = (
+    ("Select bank1", "Select bank1"),
+    ("Access Bank", "Access Bank"),
+    ("Cal Bank", "Cal Bank"),
+    ("Fidelity Bank", "Fidelity Bank"),
+    ("Ecobank", "Ecobank"),
+    ("Pan Africa", "Pan Africa"),
+    ("First Bank of Nigeria", "First Bank of Nigeria"),
+    ("SGSSB", "SGSSB")
+)
+
+BANKS2 = (
+    ("Select bank2", "Select bank2"),
     ("Access Bank", "Access Bank"),
     ("Cal Bank", "Cal Bank"),
     ("Fidelity Bank", "Fidelity Bank"),
@@ -84,8 +95,18 @@ MODE_OF_PAYMENT2 = (
     ("Cash left @", "Cash left @"),
 )
 
-PAYMENT_OFFICES = (
-    ("Please select cash at location", "Please select cash at location"),
+PAYMENT_OFFICES1 = (
+    ("Please select cash at location1", "Please select cash at location1"),
+    ("Cash @ location", "Cash @ location"),
+    ("DVLA", "DVLA"),
+    ("HEAD OFFICE", "HEAD OFFICE"),
+    ("KEJETIA", "KEJETIA"),
+    ("ECOBANK", "ECOBANK"),
+    ("PAN AFRICA", "PAN AFRICA"),
+)
+
+PAYMENT_OFFICES2 = (
+    ("Please select cash at location2", "Please select cash at location2"),
     ("Cash @ location", "Cash @ location"),
     ("DVLA", "DVLA"),
     ("HEAD OFFICE", "HEAD OFFICE"),
@@ -152,7 +173,7 @@ class CustomerAccounts(models.Model):
     agent = models.ForeignKey(User, on_delete=models.CASCADE)
     account_number = models.CharField(max_length=16)
     account_name = models.CharField(max_length=100)
-    bank = models.CharField(max_length=100, choices=BANKS, default="Access Bank")
+    bank = models.CharField(max_length=100, choices=BANKS1, default="Access Bank")
     phone = models.CharField(max_length=15)
     date_added = models.DateTimeField(auto_now_add=True)
 
@@ -176,7 +197,7 @@ class BankDeposit(models.Model):
     guarantor = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     customer = models.CharField(max_length=20, blank=True)
     agent = models.ForeignKey(User, on_delete=models.CASCADE, related_name="agent_requesting_bank")
-    bank = models.CharField(max_length=50, choices=BANKS, blank=True, default="")
+    bank = models.CharField(max_length=50, choices=BANKS1, blank=True, default="")
     account_number = models.TextField(blank=True,max_length=17)
     account_name = models.CharField(max_length=100, blank=True, default="")
     amount = models.DecimalField(max_digits=19, decimal_places=2, blank=True)
@@ -261,7 +282,7 @@ class UserMobileMoneyAccountsClosed(models.Model):
 class CustomerWithdrawal(models.Model):
     agent = models.ForeignKey(User, on_delete=models.CASCADE)
     customer = models.CharField(max_length=100)
-    bank = models.CharField(max_length=100, choices=BANKS, default="GT Bank")
+    bank = models.CharField(max_length=100, choices=BANKS1, default="GT Bank")
     type = models.CharField(max_length=30,choices=WITHDRAW_TYPES)
     amount = models.DecimalField(max_digits=19, decimal_places=2)
     date_requested = models.DateTimeField(auto_now_add=True)
@@ -273,10 +294,10 @@ class Payments(models.Model):
     agent = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     mode_of_payment1 = models.CharField(max_length=30, choices=MODE_OF_PAYMENT1, blank=True)
     mode_of_payment2 = models.CharField(max_length=30, choices=MODE_OF_PAYMENT2, blank=True)
-    cash_at_location1 = models.CharField(max_length=30, choices=PAYMENT_OFFICES, blank=True, default="")
-    cash_at_location2 = models.CharField(max_length=30, choices=PAYMENT_OFFICES, blank=True, default="")
-    bank1 = models.CharField(max_length=50, choices=BANKS, blank=True)
-    bank2 = models.CharField(max_length=50, choices=BANKS, blank=True)
+    cash_at_location1 = models.CharField(max_length=40, choices=PAYMENT_OFFICES1, blank=True, default="")
+    cash_at_location2 = models.CharField(max_length=40, choices=PAYMENT_OFFICES2, blank=True, default="")
+    bank1 = models.CharField(max_length=50, choices=BANKS1, blank=True)
+    bank2 = models.CharField(max_length=50, choices=BANKS2, blank=True)
     amount1 = models.DecimalField(max_digits=19, decimal_places=2,blank=True,default=0.0)
     amount2 = models.DecimalField(max_digits=19, decimal_places=2,blank=True,default=0.0)
     transaction_id1 = models.CharField(max_length=30, blank=True)
