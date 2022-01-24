33e91facf92e7aeb32ae99fb0e52906ef2ee0d8a
gabrielstonedelza <gabrielstonedelza@gmail.com>
2022-01-24 13:29:07 +0000

    edited users.py file

--- users/models.py
+++ users/models.py
@@ -5,16 +5,6 @@ from PIL import Image
 
 DeUser = settings.AUTH_USER_MODEL
 
-BANKS = (
-    ("Access Bank", "Access Bank"),
-    ("Cal Bank", "Cal Bank"),
-    ("Fidelity Bank", "Fidelity Bank"),
-    ("Ecobank", "Ecobank"),
-    ("First Bank of Nigeria", "First Bank of Nigeria"),
-    ("SGSSB", "SGSSB")
-)
-
-
 class User(AbstractUser):
     email = models.EmailField(unique=True, max_length=255)
     phone = models.CharField(max_length=15, unique=True, help_text="please format should be +233")
