a9df7df8a716b328d635bdec72e376d95640838d
gabrielstonedelza <gabrielstonedelza@gmail.com>
2022-01-26 12:50:19 +0000

    view all users deposits

--- fnet_api/views.py
+++ fnet_api/views.py
@@ -844,16 +844,14 @@ def approve_cash_deposit_paid(request, id):
 @api_view(['GET'])
 @permission_classes([permissions.IsAuthenticated])
 def get_unpaid_cash_deposits_for_today(request):
-    my_date = datetime.today()
-    your_cash_requests = CashDeposit.objects.filter(agent=request.user).filter(date_requested=my_date.date()).filter(deposit_paid="Not Paid").filter(request_status="Approved").order_by('-date_requested')
+    your_cash_requests = CashDeposit.objects.filter(agent=request.user).filter(deposit_paid="Not Paid").filter(request_status="Approved").order_by('-date_requested')
     serializer = CashDepositSerializer(your_cash_requests, many=True)
     return Response(serializer.data)
 
 @api_view(['GET'])
 @permission_classes([permissions.IsAuthenticated])
 def get_unpaid_bank_deposits_for_today(request):
-    my_date = datetime.today()
-    your_cash_requests = BankDeposit.objects.filter(agent=request.user).filter(date_requested=my_date.date()).filter(deposit_paid="Not Paid").filter(request_status="Approved").order_by('-date_requested')
+    your_cash_requests = BankDeposit.objects.filter(agent=request.user).filter(deposit_paid="Not Paid").filter(request_status="Approved").order_by('-date_requested')
     serializer = BankDepositSerializer(your_cash_requests, many=True)
     return Response(serializer.data)
 
