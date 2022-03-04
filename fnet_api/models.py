58a78ddfe83eca877901eb22942a4d3a1d15ce87
gabrielstonedelza <gabrielstonedelza@gmail.com>
2022-03-04 09:56:43 +0000

    added id type to customer withdrawals

--- fnet_api/models.py
+++ fnet_api/models.py
@@ -297,7 +297,7 @@ class CustomerWithdrawal(models.Model):
     agent = models.ForeignKey(User, on_delete=models.CASCADE)
     customer = models.CharField(max_length=100)
     bank = models.CharField(max_length=100, choices=BANKS, default="GT Bank")
-    id_type = models.CharField(max_length=20, choices=ID_TYPES, default="Passport")
+    id_type = models.CharField(max_length=20, choices=ID_TYPES)
     amount = models.DecimalField(max_digits=19, decimal_places=2)
 
     date_requested = models.DateTimeField(auto_now_add=True)
