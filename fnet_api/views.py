102b856f0b18c16683e1fa5ad382a3d8a7cb9e79
gabrielstonedelza <gabrielstonedelza@gmail.com>
2022-08-14 20:04:43 +0000

    fnet api,function to get customer deposit transactions

--- fnet_api/views.py
+++ fnet_api/views.py
@@ -1422,20 +1422,19 @@ def get_customer_requests_notifications(request):
 
 
 # get customers deposit transactions
-# class GetCustomersDepositTransactions(generics.ListAPIView):
-#     permission_classes = [permissions.AllowAny]
-#     queryset = BankDeposit.objects.all().order_by('-date_requested')
-#     serializer_class = BankDepositSerializer
-#     filter_backends = [filters.SearchFilter]
-#     search_fields = ['customer', 'date_requested', ]
-
-
 class GetCustomersDepositTransactions(generics.ListAPIView):
     permission_classes = [permissions.AllowAny]
+    queryset = BankDeposit.objects.all().order_by('-date_requested')
     serializer_class = BankDepositSerializer
     filter_backends = [filters.SearchFilter]
     search_fields = ['customer', 'date_requested', ]
 
-    def get_queryset(self, customer):
-        customer = customer
-        return BankDeposit.objects.filter(customer=customer).order_by('-date_requested')
+# class GetCustomersDepositTransactions(generics.ListAPIView):
+#     permission_classes = [permissions.AllowAny]
+#     serializer_class = BankDepositSerializer
+#     filter_backends = [filters.SearchFilter]
+#     search_fields = ['customer', 'date_requested', ]
+#
+#     def get_queryset(self, customer):
+#         customer = customer
+#         return BankDeposit.objects.filter(customer=customer).order_by('-date_requested')
