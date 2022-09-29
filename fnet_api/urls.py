384f5dd450b2688c8f19916a5a821c2a0d3ebce2
gabrielstonedelza <gabrielstonedelza@gmail.com>
2022-09-29 14:48:58 +0000

    fnet api,functions to get customer points

--- fnet_api/urls.py
+++ fnet_api/urls.py
@@ -218,8 +218,6 @@ urlpatterns = [
 
     #   customer points
     path("get_customer_points/<str:customer_phone>/", views.get_customer_points),
-    path("get_customer_redeemed_points/<str:customer_phone>/", views.get_customer_redeemed_points)
+    path("get_customer_redeemed_points/<str:customer_phone>/", views.get_customer_redeemed_points),
     path("add_to_customer_redeemed_points/", views.add_to_customer_redeemed_points)
-
-
 ]
