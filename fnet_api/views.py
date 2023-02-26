38d7155dfb34876ebd95483df154fa1d999b9b44
Gabriel Akwasi Asare <gabrielstonedelza@gmail.com>
2023-02-26 11:04:25 +0000

    fnet api,added function to search for users transactions by date

--- fnet_api/views.py
+++ fnet_api/views.py
@@ -1918,7 +1918,6 @@ def get_agents_gt_bank(request, username):
 @permission_classes([permissions.AllowAny])
 def get_agents_eco_bank_by_date(request, username,date_from,date_to):
     user = get_object_or_404(User, username=username)
-    bank = BankDeposit.objects.filter(agent=user).filter(bank="Ecobank").filter(date_requested=date_from).filter(date_requested=date_to).order_by(
-        '-date_requested')
+    bank = BankDeposit.objects.filter(agent=user).filter(bank="Ecobank").filter(date_requested=date_from).filter(date_requested=date_to)
     serializer = BankDepositSerializer(bank, many=True)
     return Response(serializer.data)
