ec9cfd3c5f179180201837b52194a391b180b02d
Gabriel Akwasi Asare <gabrielstonedelza@gmail.com>
2023-09-21 11:04:49 +0000

    fnet api,added close account to model

--- close_account/urls.py
+++ close_account/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path("close_accounts/",views.close_accounts),
-    path("get_my_closed_accounts/",views.close_accounts),
+    path("get_my_closed_accounts/",views.get_my_closed_accounts),
     path("update_my_accounts_detail/<int:pk>/",views.update_my_accounts_detail),
     path("delete_account/<int:id>/",views.delete_account),
 ]
