a76e65c9459a2f6e223efb55bb687766b742458b
Gabriel Akwasi Asare <gabrielstonedelza@gmail.com>
2023-11-28 13:58:08 +0000

    Fnet customer app, first production test

--- fnet_api/models.py
+++ fnet_api/models.py
@@ -162,8 +162,8 @@ CUSTOMER_REFERRAL_STATUS = (
 )
 
 class CashSupportRequest(models.Model):
-    customer_phone = models.CharField(max_length=20)
-    customer_name = models.CharField(max_length=50)
+    customer_phone = models.CharField(max_length=20,blank=True)
+    customer_name = models.CharField(max_length=50,blank=True)
     amount = models.DecimalField(max_digits=19, decimal_places=2, blank=True)
     status = models.CharField(max_length=100,choices=REQUEST_STATUS,default="Pending")
     date_requested = models.DateTimeField(auto_now_add=True)
